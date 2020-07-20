7c259598bf563aa38b2f67f4a0e2f782daa86500
Fernando <fdoidera@gmail.com>
2020-07-20 20:59:03 -0300

    mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import api from './services/api';
+import './App.scss';
 import Header from './components/Header';
-import api from './services/api'; 
-import './App.css';
+import Select from './components/Select/Select';
 
 export default function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -18,19 +18,27 @@ module.exports = {
           loader: 'babel-loader',
         }
       },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ]
-      },
+      // {
+      //   test: /\.css$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     { loader: 'style-loader' },
+      //     { loader: 'css-loader' },
+      //   ]
+      // },
       {
         test: /.*\.(gif|png|jpe?g)$/i,
         use: {
           loader: 'file-loader',
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
       }
     ]
   }
